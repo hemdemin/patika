b3ef6a5f682ca0123731167675785c2385d0f69d
hemdemin <98806628+hemdemin@users.noreply.github.com>
2022-02-01 14:50:50 +0300

    Add files via upload

---
+++ Patika_bitirme_projesi.py
@@ -0,0 +1,20 @@
+
+
+
+List_1 = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+
+a = List_1[0][0]
+b = List_1[0][1]
+c = List_1[0][2][0]
+d=  List_1[0][3]
+e = List_1[1][0][0][0]
+f=  List_1[1][1]
+g=  List_1[2]
+h=  List_1[3]
+
+
+List_2 =[a,b,c,d,e,f,g,h]
+print(List_2)
+
+               
+
---
+++ patika_bitirme_2.py
@@ -0,0 +1,22 @@
+
+
+
+List_1 = [[1, 2], [3, 4], [5, 6, 7]]
+List_1.reverse()
+
+a= List_1 [0]
+a.reverse()
+
+b= List_1 [1]
+b.reverse()
+
+c= List_1 [2]
+c.reverse()
+
+
+
+List_final = [a,b,c]
+print(List_final)
+
+
+
